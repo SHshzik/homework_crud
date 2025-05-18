5bbb9cdfceb244b83102455152809e6661b2d8dc
Andrey Salnikov <andrycalnicow@gmail.com>
2025-05-18 13:35:19 +0400

    Add support http create

--- services/user-client/adapters/http_adapter.go
+++ services/user-client/adapters/http_adapter.go
@@ -1,6 +1,7 @@
 package adapters
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -56,7 +57,40 @@ func (c *HTTPClient) Index() ([]*entity.User, error) {
 }
 
 func (c *HTTPClient) Create(name, email, phone string) (*entity.User, error) {
-	return nil, nil
+	url := fmt.Sprintf("http://localhost:%d/v1/users", c.cfg.PORT)
+	data, err := json.Marshal(entity.User{Name: name, Email: email, Phone: phone})
+	if err != nil {
+		c.l.Error("fail to marshal user: %v", err)
+
+		return nil, err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		c.l.Error("fail to create user: %v", err)
+
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.l.Error("fail to read body: %v", err)
+
+		return nil, err
+	}
+
+	var user entity.User
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		c.l.Info("body: %v", string(body))
+		c.l.Error("fail to unmarshal user: %v", err)
+
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (c *HTTPClient) Read(id int) (*entity.User, error) {
